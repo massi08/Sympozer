cb8a74adead798962632d1a696e5d357ca69f423
Massi <m.bouneffa@upfit-services.com>
2017-02-04 19:16:36 +0100

    Style search component

--- app/src/app/modules/schedule/schedule.component.ts
+++ app/src/app/modules/schedule/schedule.component.ts
@@ -1,27 +1,29 @@
-import { Component, OnInit} from '@angular/core';
-import { ActivatedRoute, Params }   from '@angular/router';
-import { Location }              from '@angular/common';
-import { LocalDAOService } from  '../../localdao.service';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Params}   from '@angular/router';
+import {Location}              from '@angular/common';
+import {LocalDAOService} from  '../../localdao.service';
 
 
 @Component({
-  selector: 'schedule',
-  templateUrl: 'schedule.component.html'
+    selector: 'schedule',
+    templateUrl: 'schedule.component.html',
+    styleUrls: ['./schedule.component.scss']
 })
-export class ScheduleComponent implements OnInit{
-  schedule;
-  constructor(private location: Location,
-  private route : ActivatedRoute,
-  private DaoService : LocalDAOService) {
-  }
+export class ScheduleComponent implements OnInit {
+    schedule;
 
-  ngOnInit() {
-    // this.schedule = this.DaoService.query("getConferenceSchedule", null);
-    console.log(this.schedule);
+    constructor(private location: Location,
+                private route: ActivatedRoute,
+                private DaoService: LocalDAOService) {
+    }
 
-    /*this.route.params.forEach((params: Params) => {
-  		let id = params['id'];
-  		console.log("id : " + id); 
-  	});*/
-  }
+    ngOnInit() {
+        // this.schedule = this.DaoService.query("getConferenceSchedule", null);
+        console.log(this.schedule);
+
+        /*this.route.params.forEach((params: Params) => {
+         let id = params['id'];
+         console.log("id : " + id);
+         });*/
+    }
 }
