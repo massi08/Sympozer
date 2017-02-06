69e28e40baa84846f96bd2eb836c48b97fab54d7
Massi <m.bouneffa@upfit-services.com>
2017-02-06 15:10:41 +0100

    Improve research

--- app/src/app/modules/person-by-name/person-by-name.component.ts
+++ app/src/app/modules/person-by-name/person-by-name.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Conference} from '../../model/conference';
 import {DataLoaderService} from '../../data-loader.service';
 import {Router} from '@angular/router';
@@ -8,41 +8,54 @@ import {DBLPDataLoaderService} from "../../dblpdata-loader.service";
 import {Subject} from "rxjs";
 
 @Component({
-  selector: 'app-person-by-name',
-  templateUrl: './person-by-name.component.html',
-  styleUrls: ['./person-by-name.component.scss']
+    selector: 'app-person-by-name',
+    templateUrl: './person-by-name.component.html',
+    styleUrls: ['./person-by-name.component.scss']
 })
 export class PersonByNameComponent implements OnInit {
-  persons;
-  personsFound = [];
-  searchTerms = new Subject<string>();
-  constructor(private router: Router,
-              private dataLoaderService: DataLoaderService,
-              private DaoService: LocalDAOService,
-              private  dBPLDataLoaderService: DBLPDataLoaderService) { }
+    persons;
+    personsFound = [];
+    searchTerms = new Subject<string>();
 
-  ngOnInit() {
-    this.persons = this.DaoService.query("getAllPersons", null);
-    this.searchTerms.debounceTime(300)
-                    .distinctUntilChanged()
-                    .switchMap(term => term)
-  }
-
-  search(term: string): void {
-    console.log(term);
-    this.searchTerms.next(term);
-    this.personsFound.length = 0;
-    if (term !== ""){
-      this.findPerson(term);
+    constructor(private router: Router,
+                private dataLoaderService: DataLoaderService,
+                private DaoService: LocalDAOService,
+                private  dBPLDataLoaderService: DBLPDataLoaderService) {
     }
-    console.log(this.personsFound);
-  }
 
-  findPerson(term: string): void{
-    for(var key in this.persons){
-      if(this.persons[key].name.toUpperCase().match(term.toUpperCase())){
-        this.personsFound.push(this.persons[key]);
-      }
+    ngOnInit() {
+        this.persons = this.DaoService.query("getAllPersons", null);
+        this.searchTerms.debounceTime(300)
+            .distinctUntilChanged()
+            .switchMap(term => term)
     }
-  }
+
+    search(term: string): void {
+        this.searchTerms.next(term);
+        this.personsFound.length = 0;
+        if (term !== "") {
+            this.findPerson(term);
+        }
+    }
+
+    findPerson(term: string): void {
+        let hasMatch = false;
+        for (var key in this.persons) {
+            let match = this.persons[key].name.toUpperCase().match(term.toUpperCase());
+            if (match) {
+                hasMatch = true;
+                let personClone = JSON.parse(JSON.stringify(this.persons[key]));
+                let lastIndex = match.index + term.length;
+                personClone.name = personClone.name.slice(0, match.index) + '<b>' + personClone.name.slice(match.index, lastIndex) + '</b>' + personClone.name.slice(lastIndex);
+                this.personsFound.push(personClone);
+            }
+        }
+        if (!hasMatch && term.trim() !== "") {
+            let notFoundObject = {
+                name: "&laquo;<b>" + term + "</b>&raquo; not found.",
+                id: "none"
+            };
+            this.personsFound.push(notFoundObject);
+        }
+    }
 }
